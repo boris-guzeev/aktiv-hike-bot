507277db216ecb3a0954fe06176b3f0571838278
Boris Guzeev <borisjuzeppe@gmail.com>
2026-02-28 16:29:32 +0400

    feat(client-bot): add client main.go, handler and router

--- cmd/client-bot/main.go
+++ cmd/client-bot/main.go
@@ -1,1 +1,58 @@
-package clientbot
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"time"
+
+	"github.com/boris-guzeev/aktiv-hike-bot/internal/bot/client"
+	"github.com/boris-guzeev/aktiv-hike-bot/internal/db/sqlc"
+	tgbot "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/jackc/pgx/v5"
+)
+
+func main() {
+	// TODO: init Logger
+
+	// Init Context
+	ctx := context.Background()
+
+	// Init Location (Timezone)
+	loc, err := time.LoadLocation(os.Getenv("TZ"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Init TelegramBotAPI
+	botToken := os.Getenv("CLIENT_BOT_TOKEN")
+	bot, err := tgbot.NewBotAPI(botToken)
+	if err != nil {
+		log.Fatal(err)
+	}
+	bot.Debug = false
+
+	// Init DB
+	dbDsn := os.Getenv("DB_DSN")
+	conn, err := pgx.Connect(ctx, dbDsn)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close(ctx)
+
+	// Init SQLC
+	queries := sqlc.New(conn)
+
+	// Init router
+	r := client.NewRouter(loc, bot, queries)
+
+	u := tgbot.NewUpdate(0)
+	u.Timeout = 30
+
+	updates := bot.GetUpdatesChan(u)
+	for upd := range updates {
+		if err := r.Route(ctx, upd); err != nil {
+			log.Printf("route error: %v", err)
+		}
+	}
+}
---
+++ internal/bot/client/router.go
@@ -0,0 +1,36 @@
+package client
+
+import (
+	"context"
+	"time"
+
+	"github.com/boris-guzeev/aktiv-hike-bot/internal/bot/client/handlers"
+	"github.com/boris-guzeev/aktiv-hike-bot/internal/db/sqlc"
+	tgbot "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+type router struct {
+	bot *tgbot.BotAPI
+	handler     *handlers.Handler
+}
+
+func NewRouter(l *time.Location, b *tgbot.BotAPI, q *sqlc.Queries) *router {
+	return &router{
+		bot:         b,
+		handler:     handlers.New(b, q),
+	}
+}
+
+func (r *router) Route(ctx context.Context, u tgbot.Update) error {
+	// Private commands
+	if m := u.Message; m != nil && m.Chat.IsPrivate() {
+		return r.handler.HandleMessage(ctx, m)
+	}
+
+	// Private callbacks
+	if q := u.CallbackQuery; q != nil && q.Message.Chat.IsPrivate() {
+		return r.handler.HandleCallback(ctx, q)
+	}
+
+	return nil
+}
